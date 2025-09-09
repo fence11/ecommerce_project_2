09e54a7296bcc2f621e2561481d903a8b52a83b0
fence11 <mihaila.m.211@gmail.com>
2025-09-09 21:12:45 +0300

    Prisma and form added to proj:
    - added prisma ORM
    - added form inputs

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,19 +1,82 @@
-"use client";
+"use client"
 
-import { useStoreModal } from "@/hooks/use-store-modal";
-import { Modal } from "../ui/modal";
+import * as z from 'zod'
+import { Modal } from "@/components/ui/modal"
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '../ui/button';
+import { useState } from 'react';
+
+const formSchema = z.object({
+    name: z.string().min(1)
+})
+
 
 export const StoreModal = () => {
-    const StoreModal = useStoreModal();
 
+    const [loading, setLoading] = useState(false)
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: ""
+        }
+    })
+
+    const storeModal = useStoreModal();
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            setLoading(true);
+            console.log(values)    
+            console.log("Store created.")
+        } catch (err) {
+            console.log("Some error... placeholder")
+        } finally {
+            setLoading(false);
+        }
+    }
     return (
         <Modal
-        title="Create Store"
-        description="Add a new store to manage"
-        isOpen={StoreModal.isOpen}
-        onClose={StoreModal.onClose}>
-
-            Create Store Form (WIP)
+            title="Create Store"
+            description="Add a new store to manage products and categories"
+            isOpen={storeModal.isOpen}
+            onClose={storeModal.onClose}
+        >
+            <div>
+                <div className='py-2 pb-4 space-y-4'>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                disabled={loading}
+                                                placeholder='E-commerce'
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <div className='flex items-center justify-end w-full pt-6 space-x-2'>
+                                <Button
+                                    disabled={loading}
+                                    variant="outline"
+                                    onClick={storeModal.onClose}>Cancel</Button>
+                                <Button disabled={loading} type='submit' >Continue</Button>
+                            </div>
+                        </form>
+                    </Form>
+                </div>
+            </div>
         </Modal>
     )
 }
