f61b693baabfe47794e342be7086c74a55f2b230
fence11 <mihaila.m.211@gmail.com>
2025-09-11 15:05:55 +0300

    Fixed some bugs, had navbar placeholder:
    - figured out how to see db tables
    - navbar placeholder
    - fixed bug where layout.tsx wasn't being read

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,37 @@
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from "next/navigation"
+import prismadb from "@/lib/prismadb";
+import Navbar from "@/components/navbar";
+
+interface DashboardType {
+    children: React.ReactNode;
+    params: Promise<{ storeId: string }>
+}
+
+export default async function Dashboard({children, params}: DashboardType) {
+    const { userId } = await auth();
+    const { storeId } = await params;
+
+    if (!userId) {
+        redirect('/sign-in')
+    }
+
+    const store = await prismadb?.store.findFirst({
+        where: {
+            id: storeId,
+            userId
+        }
+    })
+
+    if (!store) {
+        redirect('/');
+    }
+
+    return (
+        <>
+            <h1 className='text-red-500'>NAVBAR</h1>
+            {/* <Navbar/>
+            {children} */}
+        </>
+    )
+}
